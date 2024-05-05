fa95481e8b45ba2170647b31be6e2c19fed6d8e3
Christine Gao <cgao@optinadx.com>
2024-05-04 21:27:35 -0400

    inclusion of pruning

--- learning_curve.py
+++ learning_curve.py
@@ -18,29 +18,48 @@ DECISION_TREE_MAX_DEPTHS = {
     WINE: 10,
 }
 
+DECISION_TREE_P_VALUES = {
+    ABALONE: 0.05,
+    IRIS: 0.05,
+    WINE: 0.05,
+}
+
 # Load datasets
 iris_train, iris_train_labels, iris_test, iris_test_labels = load_iris_dataset(TRAIN_RATIO)
 wine_train, wine_train_labels, wine_test, wine_test_labels = load_wine_dataset(TRAIN_RATIO)
 abalone_train, abalone_train_labels, abalone_test, abalone_test_labels = load_abalone_dataset(TRAIN_RATIO)
 
 # Classifiers - Decision Trees
-dt_abalone = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[ABALONE])
-dt_iris = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[IRIS])
-dt_wine = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[WINE])
+dt_abalone_no_pruning = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[ABALONE], pruning=False)
+dt_iris_no_pruning = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[IRIS], pruning=False)
+dt_wine_no_pruning = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[WINE], pruning=False)
+dt_abalone_pruning = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[ABALONE], pruning=True, p_value_threshold=DECISION_TREE_P_VALUES[ABALONE])
+dt_iris_pruning = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[IRIS], pruning=True, p_value_threshold=DECISION_TREE_P_VALUES[IRIS])
+dt_wine_pruning = DecisionTree(max_depth=DECISION_TREE_MAX_DEPTHS[WINE], pruning=True, p_value_threshold=DECISION_TREE_P_VALUES[WINE])
 
 # Percentages to use for learing curve
 percentages = [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 
 # Get the classifer
-def get_classifier(dataset):
-    if dataset == 'abalone':
-        return dt_abalone
-    elif dataset == 'iris':
-        return dt_iris
-    elif dataset == 'wine':
-        return dt_wine
+def get_classifier(dataset, pruning):
+    if pruning:
+        if dataset == 'abalone':
+            return dt_abalone_pruning
+        elif dataset == 'iris':
+            return dt_iris_pruning
+        elif dataset == 'wine':
+            return dt_wine_pruning
+        else:
+            raise ValueError('Error: invalid dataset')
     else:
-        raise ValueError('Error: invalid dataset')
+        if dataset == 'abalone':
+            return dt_abalone_no_pruning
+        elif dataset == 'iris':
+            return dt_iris_no_pruning
+        elif dataset == 'wine':
+            return dt_wine_no_pruning
+        else:
+            raise ValueError('Error: invalid dataset')
 
 # Get the data
 def get_data(dataset):
@@ -54,22 +73,31 @@ def get_data(dataset):
         raise ValueError('Error: invalid dataset')
 
 # Compute the learning curve
-def show_learning_curve(dataset):
+def show_learning_curve(dataset, pruning, print_results=False):
+    # Store accuracies
     accuracies = []
 
-    classifier = get_classifier(dataset)
+    # Get the classifier & the data
+    classifier = get_classifier(dataset, pruning)
     train, train_labels, test, test_labels = get_data(dataset)
+
+    # Compute the accuracy for each percentage
     for percentage in percentages:
+        # Sample the training set
         num_elements = int(len(train) * percentage / 100)
         train_sample = train[:num_elements]
         train_labels_sample = train_labels[:num_elements]
 
+        # Train
         classifier.train(train_sample, train_labels_sample)
 
+        # Evaluate and store the accuracy
         _, accuracy, _, _, _ = classifier.evaluate(test, test_labels)
         accuracies.append((percentage, accuracy))
 
-        print(f'Got {accuracy:.2f} accuracy for {percentage}% ({num_elements} samples).')
+        # Print results if needed
+        if print_results:
+            print(f'Got {accuracy:.2f} accuracy for {percentage}% ({num_elements} samples).')
 
     # Extract percentage and accuracy from the list of tuples
     x = [item[0] for item in accuracies]
@@ -78,11 +106,24 @@ def show_learning_curve(dataset):
     # Plotting
     plt.figure(figsize=(8, 6))
     plt.plot(x, y, color='blue', label='Accuracy')
-    plt.title(f'Learning curve of decision tree on {dataset}')
+    title = f'Learning curve of decision tree on {dataset}'
+    if pruning:
+        title += f' with pruning (p-value threshold: {DECISION_TREE_P_VALUES[dataset]})'
+    else:
+        title += ' without pruning'
+    plt.title(title)
     plt.xlabel('Percentage of the training set')
     plt.ylabel('Accuracy of the predictions')
     plt.grid(True)
     plt.legend()
     plt.show()
 
-show_learning_curve('abalone')
+def main():
+    datasets = [ABALONE, IRIS, WINE]
+    pruning = [False, True]
+    for dataset in datasets:
+        for p in pruning:
+            show_learning_curve(dataset, p, print_results=True)
+
+if __name__ == '__main__':
+    main()
